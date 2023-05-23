50b393e85eea57959f18972aa19fb8490a72b1de
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-23 15:50:40 +0100

    E2E testing done

--- src/containers/Calculator.js
+++ src/containers/Calculator.js
@@ -93,7 +93,9 @@ function App() {
   }
 
   const divide = (number) => {
-    let calculatedNumber = parseFloat(previousTotal) / parseFloat(number);
+    let calculatedNumber 
+    if (number != 0) {calculatedNumber = parseFloat(previousTotal) / parseFloat(number)} 
+    else {calculatedNumber = "Err"}
     setRunningTotal(calculatedNumber);
     setCalculatedTotal(calculatedNumber);
   }
